e471f4610dfe3c505e567f8697b36bb65c9e6240
Kathleen Hogan <kathleen.j.hogan@gmail.com>
2019-09-18 15:27:56 -0700

    Add styling for collection preview

--- src/Components/CollectionPreview/CollectionPreview.jsx
+++ src/Components/CollectionPreview/CollectionPreview.jsx
@@ -4,7 +4,7 @@ import './CollectionPreview.scss';
 
 const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
-    <h1 className='preview-title'>{title.toUpperCase()}</h1>
+    <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
       {items
         .filter((item, i) => i < 4)
