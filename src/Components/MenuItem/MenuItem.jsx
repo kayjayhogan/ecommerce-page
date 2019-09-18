8c7471027536a160f254f170986a6395aabbf07e
Kathleen Hogan <kathleen.j.hogan@gmail.com>
2019-09-18 12:43:00 -0700

    Add menu section objects

--- src/Components/MenuItem/MenuItem.jsx
+++ src/Components/MenuItem/MenuItem.jsx
@@ -4,7 +4,7 @@ import './MenuItem.scss';
 const MenuItem = ({ title }) => (
   <div className='menu-item'>
     <div className='content'>
-      <h1 className='title'>{title}</h1>
+      <h1 className='title'>{title.toUpperCase()}</h1>
       <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
