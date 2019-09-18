c5f95e64bb46e31225e48ae208e85c0c13c73f53
Kathleen Hogan <kathleen.j.hogan@gmail.com>
2019-09-18 15:24:50 -0700

    Add component for each collection item

--- src/Components/CollectionPreview/CollectionPreview.jsx
+++ src/Components/CollectionPreview/CollectionPreview.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CollectionItem from '../CollectionItem/CollectionItem.jsx';
 import './CollectionPreview.scss';
 
 const CollectionPreview = ({ title, items }) => (
@@ -7,8 +8,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className='preview'>
       {items
         .filter((item, i) => i < 4)
-        .map(item => (
-          <div key={item.id}>{item.name}</div>
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps}/>
         ))}
     </div>
   </div>
