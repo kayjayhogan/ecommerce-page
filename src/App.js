ff6763fccc3d23b20007a9dbfbe13f547a93711f
Kathleen Hogan <kathleen.j.hogan@gmail.com>
2019-09-18 17:19:32 -0700

    Create login/signup page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './Pages/HomePage/HomePage.jsx';
 import ShopPage from './Pages/ShopPage/ShopPage.jsx';
+import AuthPage from './Pages/AuthPage/AuthPage.jsx';
 import Header from './Components/Header/Header.jsx';
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/login' component={AuthPage} />
       </Switch>
     </div>
   );
---
+++ src/Pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AuthPage = () => (
+  <div className='auth-page'>SIGN IN</div>
+);
+
+export default AuthPage;
